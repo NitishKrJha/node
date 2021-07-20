df9fa46a415358048760f988bdf5cf9951850261
escalierssolution <nitish.jha@mjunction.in>
2021-07-21 00:13:03 +0530

    update

--- src/config/constraint.js
+++ src/config/constraint.js
@@ -1,9 +1,9 @@
 module.exports = Object.freeze({
     PRODUCTION: false,
-    FILE_UPLOAD_PATH: "../crmrunner/",
+    FILE_UPLOAD_PATH: "../grubbuddies/",
     SITE_URL: "http://localhost",
     SITE_PORT: "4400",
     SITE_PROTOCOL: "http",
-    AUTHORIZE_LOGIN_KEY: "4Gr3csTt4Kt",
-    AUTHORIZE_TRANSACTION_KEY: "5mg85b2C89BZzc9f",
+    AUTHORIZE_LOGIN_KEY: "4Gr3csTt4KtTT",
+    AUTHORIZE_TRANSACTION_KEY: "5mg85b2C89BZzc9f11",
 });
